1b17598d38f89b9f29bfd8f542b98dd63073a09c
AdreN167 <zz11926557@gmail.com>
2021-02-08 22:22:00 +0600

    Дополнение к меню.

--- TransportSystem/Menu.cs
+++ TransportSystem/Menu.cs
@@ -1,32 +1,60 @@
-﻿using System;
+﻿using Abstract;
+using Models;
+using System;
 
 
 namespace UI
 {
     public class Menu
     {
-        private string[] items;
+        public string[] Items { get; set; }
+        public Terminal Terminal { get; set; }
+        public Card Card { get; set; }
 
         public Menu() { }
         public Menu(string[] items)
         {
-            this.items = items;
+            Items = items;
         }
 
 
         public void Input(string[] items)
         {
-            this.items = items;
+            Items = items;
         }
 
         public void Print()
         {
             Console.WriteLine("\n\n\n\t\t-----------Меню--------------");
-            for (int i = 0; i < items.Length; i++)
+            for (int i = 0; i < Items.Length; i++)
             {
-                Console.WriteLine($"\t\t{i + 1}. {items[i]}");
+                Console.WriteLine($"\t\t{i + 1}. {Items[i]}");
             }
             Console.Write("\n\t -->");
+        }
+
+        public bool MainMenu(int choice)
+        {
+            bool result;
+            switch (choice)
+            {
+                case 1:
+                    Terminal.Validation(Card, out string message);
+                    result = true;
+                    break;
+                case 2:
+                    Terminal.ToUpBalance(Card, 5000);
+                    result = true;
+                    break;
+                case 3:
+                    Terminal.GetInformation(Card);
+                    result = true;
+                    break;
+                default:
+                    result = false;
+                    break;
+            }
+            return result;
         }
     }
 }
--- TransportSystem/Program.cs
+++ TransportSystem/Program.cs
@@ -9,35 +9,36 @@ namespace UI
         static void Main(string[] args)
         {
             string[] items = { "Валидация", "Пополнить баланс", "Информация" };
-            var menu = new Menu(items);
-            var terminal = new Terminal();
-            Card card = new UserCard() { Balance = 5000, BarcodeCard = "shtrih", IsWork = true, TariffPlans = Enum.TariffPlans.OneTripPrace, UserStatus = Enum.UserStatus.Child, СardNumber = "123123123" };
 
+            Console.Write("Введите номер карты: ");
+            string cardNumber = Console.ReadLine();
 
-            string choice;
+            Card card = new UserCard
+            {
+                Balance = 5000,
+                BarcodeCard = "shtrih",
+                IsWork = true,
+                TariffPlans = Enum.TariffPlans.OneTripPrace,
+                UserStatus = Enum.UserStatus.Child,
+                СardNumber = cardNumber
+            };
+
+            var menu = new Menu {
+                Items = items,
+                Terminal = new Terminal(),
+                Card = card
+            };
+
+            int choice;
 
             do
             {
                 Console.Clear();
                 menu.Print();
-                choice = Console.ReadLine();
+                
+                int.TryParse(Console.ReadLine(), out choice);
 
-                switch (choice)
-                {
-
-                    case "1":
-                        terminal.Validation(card, out string message);
-                        break;
-                    case "2":
-                        terminal.ToUpBalance(card, 5000);
-                        break;
-                    case "3":
-                        terminal.GetInformation(card);
-                        break;
-                }
-
-                Console.ReadKey();
-            } while (choice != "3");
+            } while (menu.MainMenu(choice));
         }
     }
 }
