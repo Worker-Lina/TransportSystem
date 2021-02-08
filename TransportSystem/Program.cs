f9da24703225873b3cd8f66ca95a4e1bd4cdd180
алина человечкова <alinasmart19@mail.ru>
2021-02-08 19:59:58 +0600

    Добавьте файлы проекта.

---
+++ TransportSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TransportSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
